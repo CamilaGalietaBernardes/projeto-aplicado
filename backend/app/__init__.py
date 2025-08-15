85cbe2f1f75d6568d3ed64e954cb5862c28f48d2
CamilaGalietaBenardes <camilagalietaber@hotmail.com>
2025-08-15 16:16:18 -0300

    Ajustes na criação do banco de dados

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,36 +1,47 @@
-from dotenv import load_dotenv
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-import os
 from flask_cors import CORS
 
+from dotenv import load_dotenv
+load_dotenv(override=False)
 
 db = SQLAlchemy()
 
 
-def create_app():
-    load_dotenv()
-    print("DEBUG:", os.getenv("DB_HOST"))
+def create_app() -> Flask:
 
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = (
-        f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    DB_USER = os.getenv('DB_USER', 'camila_pa')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'projeto_aplicado')
+    DB_HOST = os.getenv('DB_HOST', 'db')
+    DB_PORT = os.getenv('DB_PORT', '5432')
+    DB_NAME = os.getenv('DB_NAME', 'gestao_estoque_manutencao')
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
 
-    CORS(app, resources={r"/*": {"origins": ["http://localhost:5173"]}}, 
-     methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-     allow_headers=["Content-Type", "Authorization"])
+    app.config["SQLALCHEMY_ECHO"] = bool(int(os.getenv("SQLALCHEMY_ECHO", "0")))
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    CORS(
+        app,
+        resources={r"/*": {"origins": ["http://localhost:5173"]}},
+        methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allow_headers=["Content-Type", "Authorization"],
+    )
 
     db.init_app(app)
 
+    from .routes import routes
+    app.register_blueprint(routes.bp)
+
+    from .models import models as _models
+
     with app.app_context():
-        from .routes import routes
-        app.register_blueprint(routes.bp)
+        db.create_all() 
+        print("Tabelas criadas com sucesso!")
 
     return app
