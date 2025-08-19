d8418ec6c97dc3be719d6fa3d0e45d2b7166fda7
cristianbrunone <brunonecordero@gmail.com>
2025-08-19 15:23:40 -0300

    fix: dependencias flutter

--- mobile/ios/Flutter/flutter_export_environment.sh
+++ mobile/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/cristianbrunone/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/cristianbrunone/Documents/projeto-aplicado/mobile"
+export "FLUTTER_APPLICATION_PATH=/Users/cristianbrunone/development/projeto-aplicado/mobile"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/cristianbrunone/Documents/projeto-aplicado/mobile/lib/main.dart"
+export "FLUTTER_TARGET=/Users/cristianbrunone/development/projeto-aplicado/mobile/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -12,4 +12,4 @@ export "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuMQ==,RkxVVFRFUl9DSEFOTkVMPXN0YW
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/cristianbrunone/Documents/projeto-aplicado/mobile/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/cristianbrunone/development/projeto-aplicado/mobile/.dart_tool/package_config.json"
