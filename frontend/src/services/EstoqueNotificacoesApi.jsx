105143abb897cf08a3f2ca6b7bef68cb7c27797f
cristianbrunone <brunonecordero@gmail.com>
2025-09-27 20:34:51 -0300

    fix: foi corrigido url da api da nuvem

--- frontend/src/services/EstoqueNotificacoesApi.jsx
+++ frontend/src/services/EstoqueNotificacoesApi.jsx
@@ -1,6 +1,6 @@
 export const buscarNotificacoesEstoque = async () => {
   try {
-    const res = await fetch("http://localhost:5000/notificacoes-estoque");
+    const res = await fetch("https://projeto-aplicado.onrender.com");
     if (!res.ok) throw new Error("Erro ao buscar notificações de estoque");
     const dados = await res.json();
     return dados;
