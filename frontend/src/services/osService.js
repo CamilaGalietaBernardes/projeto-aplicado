901418b5d7acdde1ce06c3bb32d589cd3fe16404
cristianbrunone <brunonecordero@gmail.com>
2025-09-27 17:10:14 -0300

    foi colocado a url da api da nuvem

--- frontend/src/services/osService.js
+++ frontend/src/services/osService.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = "https://projeto-aplicado.onrender.com";
 
 export async function listarOS() {
   const res = await fetch(`${API_URL}/ordemservico`);
